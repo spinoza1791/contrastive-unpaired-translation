3be128f94c6b6583d9207003d0b63813ec4a0655
ANDREW CRATON <andrew.craton@gmail.com>
2022-12-07 11:33:31 -0600

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -71,10 +71,10 @@ class Upsample2(nn.Module):
 
 
 class Upsample(nn.Module):
-    def __init__(self, channels, pad_type='repl', filt_size=4, filt_odd: float, pad_size: float, stride=2, off: float):
+    def __init__(self, channels, pad_type='repl', filt_size=4, stride=2):
         super(Upsample, self).__init__()
         self.filt_size = filt_size
-        self.filt_odd = np.mod(filt_size, 2) == 1
+        self.filt_odd = 1 #np.mod(filt_size, 2) == 1
         self.pad_size = int((filt_size - 1) / 2)
         self.stride = stride
         self.off = int((self.stride - 1) / 2.)
